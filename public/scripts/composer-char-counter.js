e6dde9ca8444678c38a9a15513f9d701a22caece
James Brown <jamesraymondbrown@gmail.com>
2022-12-06 21:06:06 +0000

    update character count feature to turn red when charcount is below 0

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -7,20 +7,24 @@
 $(document).ready(function() {
   // --- our code goes here ---
 
+  // Target the textarea so we can update the character counter
   $('textarea').on('input propertychange', function() {
-    //console.log(140 - $(this).val().length);
-  
-    //A function to calculate the number of characters remaining that can be inputted
-    const charsRemaining = 140 - $(this).val().length
 
-    //Access the "counter" output element to change its value as a user types
+    // A function to calculate the number of characters remaining that can be inputted
+    const charsRemaining = 140 - $(this).val().length;
+
+    // Access the "counter" output element to change its value as a user types
     $(this).parent().children('.tweet-button-area').children('output').val(charsRemaining);
 
-
-  })
-
-  console.log('the char counter file is linked! And the document ready function is working')
-  console.log()
+    // If the charcount becomes negative, apply new class to the output, so the number turns red on the page
+    if (charsRemaining < 0) {
+      $(this).parent().children('.tweet-button-area').children('output').addClass('negative-character-count');
+    }; 
+    // If charcount goes above -1, remove that class
+    if (charsRemaining > -1 && $(this).parent().children('.tweet-button-area').children('output').hasClass('negative-character-count')) {
+      $(this).parent().children('.tweet-button-area').children('output').removeClass('negative-character-count')
+    }
+  });
 });
 
 
