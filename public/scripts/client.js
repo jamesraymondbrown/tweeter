202360b41e764b0d7746f7c6dfb27a62df8c0801
James Brown <jamesraymondbrown@gmail.com>
2022-12-07 22:54:07 +0000

    add ajax call to display tweets from /tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,30 +6,32 @@
 
 $(document).ready(function() {
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
+  // const data = [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png"
+  //       ,
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //       "handle": "@rd" },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1461113959088
+  //   }
+  // ];
+
+  
   
   // This function loops through an array of tweet objects
   // and adds them as html to our index.html file
@@ -74,6 +76,16 @@ $(document).ready(function() {
     return $tweetInHTML;
   };
 
+  // Ajax request to display tweets on app main page
+  const loadTweets = function() {
+    $.ajax('/tweets', { method: 'GET' })
+    .then(function (displayTweets) {
+      renderTweets(displayTweets);
+    });
+  }
+
+  loadTweets();
+
   // run the renderTweets function to display the stored data as tweets on our home page
-  renderTweets(data);
+  // renderTweets(data);
 });
