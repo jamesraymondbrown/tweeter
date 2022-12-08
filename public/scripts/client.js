5830ff96ed1b925e5c0b64de549a171ee46c24ad
James Brown <jamesraymondbrown@gmail.com>
2022-12-08 22:30:18 +0000

    linted js files

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,15 +1,10 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
 $(document).ready(function() {
 
-  // This function loops through an array of tweet objects
-  // and adds them as html to our index.html file
+  // This function loops through the tweet object
+  // and adds tweets as html to our index.html file
   const renderTweets = function(tweets) {
     let $tweetHTML = "";
+    //on page load, it loops through all tweets, to display them
     if (tweets.length > 1) {
       for (const tweet of tweets) {
 
@@ -18,13 +13,13 @@ $(document).ready(function() {
 
         // appends our new HTML data to the index.html file
         $('#tweet-container').append($tweetHTML);
-      } 
+      }
     }
+    // no need to loop when only one tweet is submitted
+    // so it just grabs the newest tweet
     else {
-        // no need to loop when only one tweet is passed through
-        $tweetHTML = createTweetElement(tweets);
-
-        $('#tweet-container').append($tweetHTML);
+      $tweetHTML = createTweetElement(tweets);
+      $('#tweet-container').append($tweetHTML);
     }
   };
 
@@ -33,10 +28,10 @@ $(document).ready(function() {
   const createTweetElement = function(tweetData) {
 
     // use timeago to format render tweet date in readable format
-    const formattedTweetDate = timeago.format(tweetData.created_at); 
+    const formattedTweetDate = timeago.format(tweetData.created_at);
 
-    // turn string literals into safe t
-    const escape = function (str) {
+    // turn string literals into safe text
+    const escape = function(str) {
       let div = document.createElement("div");
       div.appendChild(document.createTextNode(str));
       return div.innerHTML;
@@ -74,40 +69,39 @@ $(document).ready(function() {
   // Ajax request to display tweets on page load
   const loadTweets = function() {
     $.ajax('/tweets', { method: 'GET' })
-    .then(function (displayTweets) {
-      renderTweets(displayTweets);
-    });
+      .then(function (displayTweets) {
+        renderTweets(displayTweets);
+      });
   }
 
-  // Steps to submitting and displaying  a tweet:
-  $( "#text-box" ).submit(function( event ) {
+  // On tweet submit:
+  $("#text-box").submit(function(event) {
     event.preventDefault();
 
     // convert our input text into a string, to check its length and value
     // found help from https://stackoverflow.com/questions/8648892/how-to-convert-url-parameters-to-a-javascript-object
-    const $inputTextSerialized = $(this).serialize();    
+    const $inputTextSerialized = $(this).serialize();
     const inputAsObject = JSON.parse('{"' + $inputTextSerialized.replace(/&/g, '","').replace(/=/g,'":"') + '"}', function(key, value) { return key===""?value:decodeURIComponent(value) });
     const inputTextAsString = inputAsObject.text;
 
 
     // Display error message if text is less than 1 or greater than 140 chars
-
     if (inputTextAsString === "" || inputTextAsString === null) {
-      const errorMessage = document.getElementById('user-input-error-message')
+      const errorMessage = document.getElementById('user-input-error-message');
       $("#user-input-error-message").slideUp("slow");
       if (!errorMessage.textContent.includes('Please')) {
         $("#user-input-error-message").append("Please write in the text box before clicking \"tweet\"");
-      };
+      }
       $("#user-input-error-message").slideDown("slow");
       return false;
     }
 
     if (inputTextAsString.length > 140) {
-      const errorMessage = document.getElementById('user-input-error-message')
+      const errorMessage = document.getElementById('user-input-error-message');
       $("#user-input-error-message").slideUp("slow");
       if (!errorMessage.textContent.includes('Tweets')) {
         $("#user-input-error-message").append("Tweets can be a maximum of 140 characters");
-      };
+      }
       $("#user-input-error-message").slideDown("slow");
       return false;
     }
@@ -117,24 +111,24 @@ $(document).ready(function() {
     //Create functions to call POST and GET the tweet with ajax
     const postTweet = () => {
       return $.ajax({
-      url: "/tweets/",
-      type: "POST",
-      data: $inputTextSerialized
-    })
+        url: "/tweets/",
+        type: "POST",
+        data: $inputTextSerialized
+      });
     };
     
     const retrieveData = () => {
       $.ajax({
-      url: '/tweets',
-      method: 'GET'
-    })
-    .then((data) => {
-      newestTweet = data[data.length-1];
-      renderTweets(newestTweet);
-    })
-    .catch((error) => {
-      console.log('error', error);
-    })
+        url: '/tweets',
+        method: 'GET'
+      })
+        .then((data) => {
+          const newestTweet = data[data.length - 1];
+          renderTweets(newestTweet);
+        })
+        .catch((error) => {
+          console.log('error', error);
+        });
     };
 
     //Call POST function, then GET data when that's done
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,3 @@
-// const testingFunction = () => {
-//   console.log('the char counter file is linked!')
-// };
-
-// testingFunction();
-
 $(document).ready(function() {
   // --- our code goes here ---
 
@@ -19,10 +13,10 @@ $(document).ready(function() {
     // If the charcount becomes negative, apply new class to the output, so the number turns red on the page
     if (charsRemaining < 0) {
       $(this).parent().children('.tweet-button-area').children('output').addClass('negative-character-count');
-    }; 
+    }
     // If charcount goes above -1, remove that class
     if (charsRemaining > -1 && $(this).parent().children('.tweet-button-area').children('output').hasClass('negative-character-count')) {
-      $(this).parent().children('.tweet-button-area').children('output').removeClass('negative-character-count')
+      $(this).parent().children('.tweet-button-area').children('output').removeClass('negative-character-count');
     }
   });
 });
